ff06a595b5482b8022541cbac7c3335e7ecf9165
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2026-04-09 11:38:22 +0200

    create BamQualityInfo earlier to reduce code duplication

--- src/wisecondorx-rs/src/lib.rs
+++ src/wisecondorx-rs/src/lib.rs
@@ -31,6 +31,21 @@ pub struct BamQualityInfo {
     pair_fail: usize,
 }
 
+impl BamQualityInfo {
+    fn new() -> Self {
+        BamQualityInfo {
+            mapped: 0,
+            unmapped: 0,
+            no_coordinate: 0,
+            filter_rmdup: 0,
+            filter_mapq: 0,
+            pre_retro: 0,
+            post_retro: 0,
+            pair_fail: 0,
+        }
+    }
+}
+
 #[pyfunction]
 #[pyo3(signature = (infile, reference, binsize, rmdup, threads))]
 pub fn wcx_convert_core(
@@ -76,18 +91,12 @@ fn process_record<R: noodles::sam::alignment::Record>(
     thread_counts: &mut [i32],
     larp: &mut isize,
     larp2: &mut isize,
-    thread_mapped: &mut usize,
-    thread_unmapped: &mut usize,
-    thread_no_coord: &mut usize,
-    thread_filter_rmdup: &mut usize,
-    thread_filter_mapq: &mut usize,
-    thread_pre_retro: &mut usize,
-    thread_pair_fail: &mut usize,
+    bqi: &mut BamQualityInfo,
 ) -> Result<()> {
     let flags = record.flags()?;
-    if flags.is_unmapped() { *thread_unmapped += 1; } else { *thread_mapped += 1; }
+    if flags.is_unmapped() { bqi.unmapped += 1; } else { bqi.mapped += 1; }
     let pos_opt = record.alignment_start().transpose()?.map(usize::from);
-    if pos_opt.is_none() { *thread_no_coord += 1; }
+    if pos_opt.is_none() { bqi.no_coordinate += 1; }
     
     let is_paired = flags.is_segmented();
     let is_proper_pair = flags.is_properly_segmented();
@@ -96,21 +105,21 @@ fn process_record<R: noodles::sam::alignment::Record>(
     let mate_pos = record.mate_alignment_start().transpose()?.map(usize::from).unwrap_or(1) as isize - 1;
 
     if is_paired {
-        if !is_proper_pair { *thread_pair_fail += 1; return Ok(()); }
-        if rmdup && *larp == pos && *larp2 == mate_pos { *thread_filter_rmdup += 1; }
+        if !is_proper_pair { bqi.pair_fail += 1; return Ok(()); }
+        if rmdup && *larp == pos && *larp2 == mate_pos { bqi.filter_rmdup += 1; }
         else if mapq >= 1 {
             let loc = (pos as usize) / binsize;
             if loc < thread_counts.len() { thread_counts[loc] += 1; }
-        } else { *thread_filter_mapq += 1; }
+        } else { bqi.filter_mapq += 1; }
 
-        *larp2 = mate_pos; *thread_pre_retro += 1; *larp = pos;
+        *larp2 = mate_pos; bqi.pre_retro += 1; *larp = pos;
     } else {
-        if rmdup && *larp == pos { *thread_filter_rmdup += 1; }
+        if rmdup && *larp == pos { bqi.filter_rmdup += 1; }
         else if mapq >= 1 {
             let loc = (pos as usize) / binsize;
             if loc < thread_counts.len() { thread_counts[loc] += 1; }
-        } else { *thread_filter_mapq += 1; }
-        *thread_pre_retro += 1; *larp = pos;
+        } else { bqi.filter_mapq += 1; }
+        bqi.pre_retro += 1; *larp = pos;
     }
     Ok(())
 }
@@ -160,14 +169,7 @@ fn process_file(
     }).collect();
 
     let results: Result<Vec<_>> = regions_list.into_par_iter().map(|(_id, chr_name, len)| {
-        let mut thread_mapped = 0;
-        let mut thread_unmapped = 0;
-        let mut thread_no_coord = 0;
-        let mut thread_filter_rmdup = 0;
-        let mut thread_filter_mapq = 0;
-        let mut thread_pre_retro = 0;
-        let mut thread_post_retro = 0;
-        let mut thread_pair_fail = 0;
+        let mut bqi = BamQualityInfo::new();
         
         let num_bins = len / binsize + 1;
         let mut thread_counts = vec![0; num_bins];
@@ -177,9 +179,7 @@ fn process_file(
         
         let region = match chr_name.parse::<noodles::core::Region>() {
             Ok(r) => r,
-            Err(_) => return Ok((chr_name, thread_counts, BamQualityInfo {
-                mapped: 0, unmapped: 0, no_coordinate: 0, filter_rmdup: 0, filter_mapq: 0, pre_retro: 0, post_retro: 0, pair_fail: 0
-            }))
+            Err(_) => return Ok((chr_name, thread_counts, bqi))
         };
 
         if ext == "bam" {
@@ -189,9 +189,7 @@ fn process_file(
                     for record in query_iter.flatten() {
                         let _ = process_record(
                             &record, rmdup, binsize, &mut thread_counts, &mut larp, &mut larp2,
-                            &mut thread_mapped, &mut thread_unmapped, &mut thread_no_coord,
-                            &mut thread_filter_rmdup, &mut thread_filter_mapq, &mut thread_pre_retro,
-                            &mut thread_pair_fail
+                            &mut bqi
                         );
                     }
                 }
@@ -203,53 +201,35 @@ fn process_file(
                         for record in query_iter.flatten() {
                             let _ = process_record(
                                 &record, rmdup, binsize, &mut thread_counts, &mut larp, &mut larp2,
-                                &mut thread_mapped, &mut thread_unmapped, &mut thread_no_coord,
-                                &mut thread_filter_rmdup, &mut thread_filter_mapq, &mut thread_pre_retro,
-                                &mut thread_pair_fail
+                                &mut bqi
                             );
                         }
                     }
                 }
 
         for &count in &thread_counts {
-            thread_post_retro += count as usize;
+            bqi.post_retro += count as usize;
         }
 
-        Ok((chr_name.clone(), thread_counts, BamQualityInfo {
-            mapped: thread_mapped, unmapped: thread_unmapped, no_coordinate: thread_no_coord,
-            filter_rmdup: thread_filter_rmdup, filter_mapq: thread_filter_mapq, pre_retro: thread_pre_retro,
-            post_retro: thread_post_retro, pair_fail: thread_pair_fail
-        }))
+        Ok((chr_name.clone(), thread_counts, bqi))
     }).collect();
 
-    let mut t_mapped = 0;
-    let mut t_unmapped = 0;
-    let mut t_no_coordinate = 0;
-    let mut t_filter_rmdup = 0;
-    let mut t_filter_mapq = 0;
-    let mut t_pre_retro = 0;
-    let mut t_post_retro = 0;
-    let mut t_pair_fail = 0;
-    
     let mut final_map = HashMap::new();
+    let mut final_bqi = BamQualityInfo::new();
     
     for (chr, counts, qual) in results? {
         final_map.insert(chr, Array1::from_vec(counts));
-        t_mapped += qual.mapped;
-        t_unmapped += qual.unmapped;
-        t_no_coordinate += qual.no_coordinate;
-        t_filter_rmdup += qual.filter_rmdup;
-        t_filter_mapq += qual.filter_mapq;
-        t_pre_retro += qual.pre_retro;
-        t_post_retro += qual.post_retro;
-        t_pair_fail += qual.pair_fail;
+        final_bqi.mapped += qual.mapped;
+        final_bqi.unmapped += qual.unmapped;
+        final_bqi.no_coordinate += qual.no_coordinate;
+        final_bqi.filter_rmdup += qual.filter_rmdup;
+        final_bqi.filter_mapq += qual.filter_mapq;
+        final_bqi.pre_retro += qual.pre_retro;
+        final_bqi.post_retro += qual.post_retro;
+        final_bqi.pair_fail += qual.pair_fail;
     }
 
-    Ok((final_map, BamQualityInfo {
-        mapped: t_mapped, unmapped: t_unmapped, no_coordinate: t_no_coordinate,
-        filter_rmdup: t_filter_rmdup, filter_mapq: t_filter_mapq, pre_retro: t_pre_retro,
-        post_retro: t_post_retro, pair_fail: t_pair_fail
-    }))
+    Ok((final_map, final_bqi))
 }
 
 #[pymodule]
