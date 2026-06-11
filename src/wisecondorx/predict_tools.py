7e786aff76a7328565826bbca66a07bbc17c5e18
Matthias De Smet <11850640+matthdsm@users.noreply.github.com>
2026-06-11 13:44:23 +0200

    move predict functions

--- src/wisecondorx/predict_tools.py
+++ src/wisecondorx/predict_tools.py
@@ -12,6 +12,13 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 import subprocess
 from wisecondorx.overall_tools import Sex
+import typer
+from wisecondorx.predict_control import get_post_processed_result
+from typing import Annotated
+from wisecondorx.predict_control import normalize
+from wisecondorx.predict_output import generate_output_tables
+from wisecondorx.plotter import write_plots
+from wisecondorx.overall_tools import gender_correct, scale_sample
 
 
 def predict_gender(sample, trained_cutoff):
@@ -457,3 +464,435 @@ def get_segment_zscore(segments_df: pd.DataFrame, cna_df: pd.DataFrame):
     segments_df["zscore"] = z_score_series
 
     return segments_df
+
+
+def wcx_predict(
+    infile: Path = typer.Argument(..., help=".npz input file"),
+    reference: Path = typer.Argument(
+        ..., help="Reference .npz, as previously created with newref"
+    ),
+    outid: str = typer.Argument(
+        ...,
+        help="Basename (w/o extension) of output files (paths are allowed, e.g. path/to/ID_1)",
+    ),
+    minrefbins: int = typer.Option(
+        150,
+        "--minrefbins",
+        help="Minimum amount of sensible reference bins per target bin.",
+    ),
+    maskrepeats: int = typer.Option(
+        5,
+        "--maskrepeats",
+        help="Regions with distances > mean + sd * 3 will be masked. Number of masking cycles.",
+    ),
+    cbs_alpha: Annotated[
+        float,
+        typer.Option(
+            min=0.0,
+            max=1.0,
+            help="p-value cut-off for calling a CBS breakpoint.",
+        ),
+    ] = 1e-4,
+    cbs_nperm: Annotated[
+        int,
+        typer.Option(
+            min=0,
+            help="Number of permutations for CBS p-value estimation. Higher is more accurate, but also slower.",
+        ),
+    ] = 10000,
+    cbs_p_method: Annotated[
+        str,
+        typer.Option(
+            "--cbs_p_method",
+            help="Method for CBS p-value estimation. See DNAcopy documentation for more details.",
+        ),
+    ] = "hybrid",
+    cbs_min_width: Annotated[
+        int,
+        typer.Option(
+            "--cbs_min_width",
+            min=2,
+            help="Minimum number of bins in a segment for it to be called by CBS. Higher is more stringent.",
+        ),
+    ] = 2,
+    cbs_kmax: Annotated[
+        int,
+        typer.Option(
+            "--cbs_kmax",
+            min=1,
+            help="Maximum width of smaller segment for permutation in the hybrid method.",
+        ),
+    ] = 25,
+    cbs_nmin: Annotated[
+        int,
+        typer.Option(
+            "--cbs_nmin",
+            min=1,
+            help="the minimum length of data for which the approximation of maximum statistic is used under the hybrid method. should be larger than 4*kmax",
+        ),
+    ] = 200,
+    cbs_eta: Annotated[
+        float,
+        typer.Option(
+            "--cbs_eta",
+            help="the probability to declare a change conditioned on the permuted statistic exceeding the observed statistic exactly j (= 1,...,nperm*alpha) times.",
+        ),
+    ] = 0.05,
+    cbs_trim: Annotated[
+        float,
+        typer.Option(
+            "--cbs_trim",
+            help="proportion of data to be trimmed for variance calculation for smoothing outliers and undoing splits based on SD.",
+        ),
+    ] = 0.025,
+    zscore: Annotated[
+        float,
+        typer.Option(help="z-score cut-off for aberration calling.", min=0.0),
+    ] = 5.0,
+    beta: Annotated[
+        float,
+        typer.Option(
+            min=0.0,
+            max=1.0,
+            help="When beta is given, --zscore is ignored and a ratio cut-off is used to call aberrations.",
+        ),
+    ] = None,
+    blacklist: Path = typer.Option(
+        None,
+        "--blacklist",
+        help="Blacklist that masks regions in output, structure of header-less file: chr...(/t)startpos(/t)endpos(/n)",
+    ),
+    gender: Sex = typer.Option(
+        None,
+        "--gender",
+        help="Force WisecondorX to analyze this case as a male (M) or a female (F)",
+    ),
+    ylim: str = typer.Option(
+        "def", "--ylim", help="y-axis limits for plotting. e.g. [-2,2]"
+    ),
+    bed: bool = typer.Option(
+        True,
+        "--bed",
+        help="Outputs tab-delimited .bed files, containing the most important information",
+    ),
+    plot: bool = typer.Option(False, "--plot", help="Output .png plots"),
+    cairo: bool = typer.Option(
+        True,
+        "--cairo",
+        help="Use cairo for plotting",
+    ),
+    add_plot_title: bool = typer.Option(
+        False,
+        "--add-plot-title",
+        help="Add the output name as plot title",
+    ),
+    cbs_seed: int = typer.Option(
+        42, "--seed", help="Seed for segmentation algorithm"
+    ),
+    regions: Path = typer.Option(
+        None,
+        "--regions",
+        help="bed file with regions to be marked on the output plot",
+    ),
+) -> None:
+    """
+    Find copy number aberrations.
+    """
+    logging.info("Starting CNA prediction")
+
+    args = argparse.Namespace()
+    args.infile = infile
+    args.reference = reference
+    args.outid = outid
+    args.sampleid = str(os.path.basename(outid))
+    args.minrefbins = minrefbins
+    args.maskrepeats = maskrepeats
+    args.cbs_seed = cbs_seed
+    args.cbs_alpha = cbs_alpha
+    args.cbs_nperm = cbs_nperm
+    args.cbs_p_method = cbs_p_method
+    args.cbs_min_width = cbs_min_width
+    args.cbs_kmax = cbs_kmax
+    args.cbs_nmin = cbs_nmin
+    args.cbs_eta = cbs_eta
+    args.cbs_trim = cbs_trim
+    args.zscore = zscore
+    args.beta = beta
+    args.blacklist = blacklist
+    args.gender = gender
+    args.ylim = ylim
+    args.bed = bed
+    args.plot = plot
+    args.cairo = cairo
+    args.add_plot_title = add_plot_title
+    args.regions = regions
+
+    if not args.bed and not args.plot:
+        logging.critical(
+            "No output format selected. "
+            "Select at least one of the supported output formats (--bed, --plot)"
+        )
+        sys.exit()
+
+    if args.zscore <= 0:
+        logging.critical(
+            "Parameter --zscore should be a strictly positive number"
+        )
+        sys.exit()
+
+    if args.beta is not None:
+        if args.beta <= 0 or args.beta > 1:
+            logging.critical(
+                "Parameter --beta should be a strictly positive number lower than or equal to 1"
+            )
+            sys.exit()
+
+    if args.cbs_alpha <= 0 or args.cbs_alpha > 1:
+        logging.critical(
+            "Parameter --cbs_alpha should be a strictly positive number lower than or equal to 1"
+        )
+        sys.exit()
+
+    logging.info("Importing data ...")
+    ref_file = np.load(args.reference, encoding="latin1", allow_pickle=True)
+    sample_file = np.load(args.infile, encoding="latin1", allow_pickle=True)
+
+    sample = sample_file["sample"].item()
+    n_reads = sum([sum(sample[x]) for x in sample.keys()])
+
+    sample = scale_sample(
+        sample, int(sample_file["binsize"].item()), int(ref_file["binsize"])
+    )
+
+    gender = (
+        args.gender
+        if args.gender
+        else predict_gender(sample, ref_file["trained_cutoff"])
+    )
+    if not ref_file["is_nipt"]:
+        sample = gender_correct(sample, gender)
+        ref_gender = gender
+    else:
+        ref_gender = Sex.FEMALE
+
+    logging.info("Normalizing autosomes ...")
+
+    results_r, results_z, results_w, ref_sizes, m_lr, m_z = normalize(
+        args, sample, ref_file, "A"
+    )
+
+    if not ref_file["is_nipt"]:
+        if not ref_file["has_male"] and gender == Sex.MALE:
+            logging.warning(
+                "This sample is male, whilst the reference is created with fewer than 5 males. "
+                "The female gonosomal reference will be used for X predictions. Note that these might "
+                "not be accurate. If the latter is desired, create a new reference and include more "
+                "male samples."
+            )
+            ref_gender = Sex.FEMALE
+
+        elif not ref_file["has_female"] and gender == Sex.FEMALE:
+            logging.warning(
+                "This sample is female, whilst the reference is created with fewer than 5 females. "
+                "The male gonosomal reference will be used for XY predictions. Note that these might "
+                "not be accurate. If the latter is desired, create a new reference and include more "
+                "female samples."
+            )
+            ref_gender = Sex.MALE
+
+    logging.info("Normalizing gonosomes ...")
+
+    null_ratios_aut_per_bin = ref_file["null_ratios"]
+    null_ratios_gon_per_bin = ref_file[
+        "null_ratios.{}".format(ref_gender.value)
+    ][len(null_ratios_aut_per_bin) :]
+
+    results_r_2, results_z_2, results_w_2, ref_sizes_2, _, _ = normalize(
+        args, sample, ref_file, ref_gender.value
+    )
+
+    rem_input = {
+        "args": args,
+        "wd": str(os.path.dirname(os.path.realpath(__file__))),
+        "binsize": int(ref_file["binsize"]),
+        "n_reads": n_reads,
+        "ref_gender": ref_gender.value,
+        "gender": gender.value,
+        "mask": ref_file["mask.{}".format(ref_gender.value)],
+        "bins_per_chr": ref_file["bins_per_chr.{}".format(ref_gender.value)],
+        "masked_bins_per_chr": ref_file[
+            "masked_bins_per_chr.{}".format(ref_gender.value)
+        ],
+        "masked_bins_per_chr_cum": ref_file[
+            "masked_bins_per_chr_cum.{}".format(ref_gender.value)
+        ],
+    }
+
+    del ref_file
+
+    results_r = np.append(results_r, results_r_2)
+    results_z = np.append(results_z, results_z_2) - m_z
+    results_w = np.append(
+        results_w * np.nanmean(results_w_2),
+        results_w_2 * np.nanmean(results_w),
+    )
+    results_w = results_w / np.nanmean(results_w)
+
+    if np.isnan(results_w).any() or np.isinf(results_w).any():
+        logging.warning(
+            "Non-numeric values found in weights -- reference too small. Circular binary segmentation and z-scoring will be unweighted"
+        )
+        results_w = np.ones(len(results_w))
+
+    ref_sizes = np.append(ref_sizes, ref_sizes_2)
+
+    null_ratios = np.array(
+        [x.tolist() for x in null_ratios_aut_per_bin]
+        + [x.tolist() for x in null_ratios_gon_per_bin],
+        dtype=object,
+    )
+
+    results = {
+        "results_r": results_r,
+        "results_z": results_z,
+        "results_w": results_w,
+        "results_nr": null_ratios,
+    }
+
+    for result in results.keys():
+        results[result] = get_post_processed_result(
+            args, results[result], ref_sizes, rem_input
+        )
+
+    log_trans(results, m_lr)
+
+    if args.blacklist:
+        logging.info("Applying blacklist ...")
+        apply_blacklist(rem_input, results)
+
+    ## Vectorize results for easier handling in post-processing and plotting
+    # vector containing chromosome number for each bin throughout the genome
+    chrom_vec = np.repeat(
+        list(range(1, len(results["results_r"]) + 1)),
+        [len(c) for c in results["results_r"]],
+    )
+    # vector containing bin number for each bin throughout the genome
+    maploc_vec = np.concatenate(
+        [np.arange(1, n + 1) for n in [len(c) for c in results["results_r"]]]
+    )
+    # vector containing start position for each bin throughout the genome
+    start_pos_vec = (maploc_vec - 1) * rem_input["binsize"]
+    # vector containing end position for each bin throughout the genome
+    end_pos_vec = maploc_vec * rem_input["binsize"]
+    # vector containing ratio for each bin
+    ratios_vec = np.concatenate(results["results_r"]).astype(float)
+    # replace 0s with NaN
+    ratios_vec[ratios_vec == 0] = np.nan
+
+    # vector containing z-score for each bin
+    zscores_vec = np.concatenate(results["results_z"]).astype(float)
+
+    # vector containing weight for each bin
+    weights_vec = np.concatenate(results["results_w"]).astype(float)
+    # replace 0s with a very small number to avoid division by zero in CBS
+    weights_vec[weights_vec == 0] = np.nextafter(0, 1)
+    if np.isnan(weights_vec).any() or np.isinf(weights_vec).any():
+        logging.warning(
+            "Non-numeric values found in weights -- reference too small. Circular binary segmentation and z-scoring will be unweighted"
+        )
+        weights_vec = np.ones(len(weights_vec))
+
+    # vectors containing mean and stdev of null ratios for each bin
+    null_ratios_mean_vec = np.array(
+        [
+            float(np.mean(bin_null_ratios))
+            for chr_null_ratios in results["results_nr"]
+            for bin_null_ratios in chr_null_ratios
+        ],
+        dtype=float,
+    )
+    null_ratios_stdev_vec = np.array(
+        [
+            float(np.std(bin_null_ratios))
+            for chr_null_ratios in results["results_nr"]
+            for bin_null_ratios in chr_null_ratios
+        ],
+        dtype=float,
+    )
+
+    cna_df = (
+        pd.DataFrame(
+            {
+                "chrom": chrom_vec,
+                "maploc": maploc_vec,
+                "start": start_pos_vec,
+                "end": end_pos_vec,
+                "null_ratios_mean": null_ratios_mean_vec,
+                "null_ratios_sd": null_ratios_stdev_vec,
+                "ratios": ratios_vec,
+                "weights": weights_vec,
+                "zscores": zscores_vec,
+            }
+        )
+        .sort_values(by=["chrom", "maploc"], ascending=[True, True])
+        .reset_index(drop=True)
+    )
+
+    logging.info("Executing circular binary segmentation ...")
+
+    # Run CBS and add z-scores per segment
+    segments_df = get_segment_zscore(exec_cbs(cna_df, args), cna_df)
+    # Rename columns, replace start and end idx with genomic positions, and sort by chromosome and start position
+    segments_df["start"] = segments_df["start"] * rem_input["binsize"]
+    segments_df["end"] = segments_df["end"] * rem_input["binsize"]
+    segments_df = segments_df.sort_values(
+        by=["chrom", "start"], ascending=[True, True]
+    ).reset_index(drop=True)
+
+    if args.bed:
+        logging.info("Writing tables ...")
+        generate_output_tables(cna_df, segments_df, args)
+
+    if args.plot:
+        logging.info("Writing plots ...")
+        data = {
+            "ref_gender": str(rem_input["ref_gender"]),
+            "beta": str(args.beta),
+            "zscore": str(args.zscore),
+            "binsize": str(rem_input["binsize"]),
+            "n_reads": str(rem_input["n_reads"]),
+            "cairo": args.cairo,
+            "cna_df": cna_df,
+            "segments_df": segments_df,
+            "ylim": str(args.ylim),
+            "regions": str(args.regions),
+            "out_dir": str(f"{args.outid}.plots"),
+        }
+
+        if args.add_plot_title:
+            data["plot_title"] = str(args.sampleid)
+
+        write_plots(data)
+
+    logging.info("Finished prediction")
+
+
+def wcx_gender(
+    infile: Path = typer.Argument(..., help=".npz input file"),
+    reference: Path = typer.Argument(
+        ..., help="Reference .npz, as previously created with newref"
+    ),
+) -> None:
+    """
+    Returns the sex of a .npz resulting from convert, based on a Gaussian mixture model trained during the newref phase.
+    """
+
+    ref_file = np.load(reference, encoding="latin1", allow_pickle=True)
+    sample_file = np.load(infile, encoding="latin1", allow_pickle=True)
+    gender = predict_gender(
+        sample_file["sample"].item(), ref_file["trained_cutoff"]
+    )
+    if gender == "M":
+        print("male")
+    else:
+        print("female")
